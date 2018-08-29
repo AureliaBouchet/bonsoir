b7ebd238c6dcf6635e1b0215b8ef00c06337f591
Romain Loeuillet <loeuillet.romain@gmail.com>
2018-08-29 14:10:26 +0200

    done placeholder

--- app/javascript/components/flatpickr.js
+++ app/javascript/components/flatpickr.js
@@ -8,13 +8,15 @@ import {French} from "flatpickr/dist/l10n/fr.js";
 flatpickr("#date", {
  altInput: true,
  altFormat: "j F Y",
+
   // dateFormat: "d-m-Y", // It creates another hidden field in your form so that November 4, 2017 is displayed to the user while the server receives database-friendly format: 2017-11-14
  // dateFormat: "d.m.Y",
  time_24hr: true,
  minDate: 'today',
+ maxDate: new Date().fp_incr(7),
  locale: French
+
  // dateFormat: "d-m-Y"
- // defaultDate: Date.today
  // plugins: [new rangePlugin({ input: "#reservation_date_end"})]
 
 });
