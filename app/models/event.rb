df8201ef403b9fc0ec88ad9892cfb00fd100b589
Aurelia Bouchet <bouchet.aurelia@gmail.com>
2018-08-30 11:04:14 +0200

    seed adjusted

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ApplicationRecord
  geocoded_by :address
+ validates :photo, presence: true
+ validates :title, presence: true
+ validates :venue, presence: true
+ validates :date, presence: true
+
  after_validation :geocode, if: :will_save_change_to_address?
 
  mount_uploader :photo, PhotoUploader
