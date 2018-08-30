576c3cab05a77dc23152aff6358bca37df2f972b
Aurelia Bouchet <bouchet.aurelia@gmail.com>
2018-08-30 17:27:15 +0200

    sorting events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,15 +4,21 @@ class EventsController < ApplicationController
     params[:date] = Date.today.strftime('%Y-%m-%d') unless params[:date].present?
 
     if params[:date].present? && params[:location].present?
-      @events = Event.where(date: params[:date]).near(params[:location], params[:distance].blank? ? 10 : params[:distance])
-    elsif params[:date].present? && params[:location].empty?
-      @events = Event.where(date: params[:date])
-    elsif params[:date].empty? && params[:location].present?
-      @events = Event.near(params[:location], params[:distance].blank? ? 10 : params[:distance])
-    else
-      @events = Event.all
-    end
+      @events = Event.where(date: params[:date]).near(params[:location], params[:distance].blank? ? 10 : params[:distance]).order(rating: :desc)
+    @events = checkAvailabilityHoursJob.perform_now(events)
 
+
+    elsif params[:date].present? && params[:location].empty?
+      @events = Event.where(date: params[:date]).sort_by(rating: :desc)
+    @events = checkAvailabilityHoursJob.perform_now(events)
+    elsif params[:date].empty? && params[:location].present?
+      @events = Event.near(params[:location], params[:distance].blank? ? 10 : params[:distance]).order(rating: :desc)
+    @events = checkAvailabilityHoursJob.perform_now(events)
+    else
+     events = Event.all.order(rating: :desc)
+     @events = checkAvailabilityHoursJob.perform_now(@events)
+    end
+raise
     @markers = @events.map do |event|
       {
         lat: event.latitude,
