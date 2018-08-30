b9d1e820b5c80b339335084421217d2d0ea5b954
Juliette Crouzet <juliettecrouzet@gmail.com>
2018-08-30 13:22:31 +0200

    marker-geolocation

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,6 +3,7 @@ import { autocomplete } from '../components/autocomplete';
 import { geolocate } from '../components/geolocation';
 geolocate();
 
+
 // const mapElement = document.getElementById('map');
 // if (mapElement) { // don't try to build a map if there's no div#map to inject in
 //   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
