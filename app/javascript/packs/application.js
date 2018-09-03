8180f8c89b182b83dcdd8592ac3c69883dd26d8b
victoire millerand <vmillerand@gmail.com>
2018-09-03 14:35:30 +0200

    slick desktop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,37 @@ formGeoloc();
 
 import { DisplayingSearchWhenClick } from  '../components/button';
 DisplayingSearchWhenClick();
+
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+$('.slick-caroussel').slick({
+ slidesToShow: 3,
+ dots: true,
+ centerMode: true,
+ // centerPadding: '40px',
+ // arrows: true,
+ // infinite:true,
+
+ responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        // centerMode: true,
+        // centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        //centerMode: true,
+        // centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
