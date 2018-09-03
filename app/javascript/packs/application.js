20d084ccdb5a81f6a16bde6854f33d9ba0aa3e0c
Aurelia Bouchet <bouchet.aurelia@gmail.com>
2018-09-03 11:57:01 +0200

    debug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import "bootstrap";
 import "../components/flatpickr";
 import { loadDynamicLogoText } from '../components/logo';
 loadDynamicLogoText();
+import { loadDynamicLogoTextNavbar } from '../components/logo';
+loadDynamicLogoTextNavbar();
 
 import { formGeoloc } from '../components/form_geoloc';
 formGeoloc();
--- app/jobs/check_availability_hours_job.rb
+++ app/jobs/check_availability_hours_job.rb
@@ -5,43 +5,77 @@ class CheckAvailabilityHoursJob < ApplicationJob
   queue_as :default
 
   def perform(events)
-
-    count = 0
     new_events = []
-      #faire le tri sur la liste des events triés selon notre algo
-      events.select do |event|
-        if count < 5
-          url_event = event.url_zoom.gsub('evt.htm', 'evtbook.htm')
-          day_event = event.date.day
-          month_event = event.date.month
-          year_event = event.date.year
 
-          html_file = open(url_event).read
-          html_doc = Nokogiri::HTML(html_file)
-          doc = html_doc.search('.calendrierhome')
-          doc_year = doc.search("[y='#{year_event}']")
-          doc_month = doc_year.search("[m='#{month_event}']")
-          doc_day = doc_month.search("[d='#{day_event}']")
-          lien = doc_day.search('a')
-          heure_string = lien.text.strip
-          # heure_time = Time.valueOf(heure_string)
-          # puts heure_string
-          # puts heure_time
-          #event.update(time: heure_string)
+    events.each do |event|
+      break if new_events.size == 5
 
-          if heure_string == ""
-            event.destroy
-            #puts "event destroyé"
-          else
-            event.update(time: heure_string)
-            count = count + 1
+      heure_string = fetch_event_hour(event)
 
-            # puts count
-            #puts "event updaté"
-          end
+      if heure_string == ""
+        event.destroy # => <Event id: 1> => truthy
+      else
+        event.update(time: heure_string)
+        new_events << event
       end
-
     end
 
+    return new_events
+
+    # #faire le tri sur la liste des events triés selon notre algo
+    # events.select do |event|
+    #   if count < 5
+    #     url_event = event.url_zoom.gsub('evt.htm', 'evtbook.htm')
+    #     day_event = event.date.day
+    #     month_event = event.date.month
+    #     year_event = event.date.year
+
+    #     html_file = open(url_event).read
+    #     html_doc = Nokogiri::HTML(html_file)
+    #     doc = html_doc.search('.calendrierhome')
+    #     doc_year = doc.search("[y='#{year_event}']")
+    #     doc_month = doc_year.search("[m='#{month_event}']")
+    #     doc_day = doc_month.search("[d='#{day_event}']")
+    #     lien = doc_day.search('a')
+    #     heure_string = lien.text.strip
+    #     # heure_time = Time.valueOf(heure_string)
+    #     # puts heure_string
+    #     # puts heure_time
+    #     #event.update(time: heure_string)
+
+    #     if heure_string == ""
+    #       event.destroy # => <Event id: 1> => truthy
+    #       false # because we're in a select
+    #       #puts "event destroyé"
+    #     else
+    #       event.update(time: heure_string)
+    #       count = count + 1
+    #       true # because we're in a select
+
+    #       # puts count
+    #       #puts "event updaté"
+    #     end
+    #   end
+    # end
+  end
+
+  private
+
+  def fetch_event_hour(event)
+    url_event = event.url_zoom.gsub('evt.htm', 'evtbook.htm')
+    day_event = event.date.day
+    month_event = event.date.month
+    year_event = event.date.year
+
+    html_file = open(url_event).read
+    html_doc = Nokogiri::HTML(html_file)
+    doc = html_doc.search('.calendrierhome')
+    doc_year = doc.search("[y='#{year_event}']")
+    doc_month = doc_year.search("[m='#{month_event}']")
+    doc_day = doc_month.search("[d='#{day_event}']")
+    lien = doc_day.search('a')
+    heure_string = lien.text.strip
+
+    return heure_string
   end
 end
