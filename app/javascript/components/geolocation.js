99ce9730c3c9097c40444d63cde932307016f151
Aurelia Bouchet <bouchet.aurelia@gmail.com>
2018-09-03 15:21:41 +0200

    debugging geoloc

--- app/javascript/components/geolocation.js
+++ app/javascript/components/geolocation.js
@@ -1,18 +1,42 @@
-import geocoder from 'geocoder';
+// import geocoder from 'geocoder';
 
 function geolocate() {
   const banner = document.getElementById('search-box-home');
   if (banner) {
     navigator.geolocation.getCurrentPosition(
       function(position) {
-        geocoder.reverseGeocode(position.coords.latitude, position.coords.longitude, function (err, data ) {
-          if (data.status === "OK") {
-            const geolocation_address = document.querySelector("#search-box-home #location");
-            geolocation_address.placeholder = data.results[0].formatted_address;
-          } else {
-            console.log(data.status);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const csrfToken = document.querySelector("meta[name='csrf-token']").content;
+
+        fetch(
+          '/geocoding',
+          {
+            method: "POST",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json",
+              "X-CSRF-Token": csrfToken
+            },
+            credentials: 'same-origin',
+            body: JSON.stringify({ lat: latitude, lng: longitude })
           }
+        )
+        .then(response => response.json())
+        .then((data) => {
+          const geolocation_address = document.querySelector("#search-box-home #location");
+          geolocation_address.placeholder = data.address;
         });
+
+        // geocoder.reverseGeocode(position.coords.latitude, position.coords.longitude, function (err, data ) {
+        //   if (data.status === "OK") {
+        //     const geolocation_address = document.querySelector("#search-box-home #location");
+        //     geolocation_address.placeholder = data.results[0].formatted_address;
+        //   } else {
+        //     console.log(data.status);
+        //   }
+        // });
       },
       function(error) {
         console.log(error);
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :events, only: [:index, :update]
 
+  resource :geocoding, only: [:create]
+
 end
